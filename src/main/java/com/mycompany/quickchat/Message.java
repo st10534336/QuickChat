838859faf795d28616fb466034a84cb093d7b54d
st10534336 <st10534336@rcconnect.edu.za>
2026-04-15 10:40:54 +0200

    added functions to Message class
    added seven methods to the Message class

--- src/main/java/com/mycompany/quickchat/Message.java
+++ src/main/java/com/mycompany/quickchat/Message.java
@@ -9,5 +9,32 @@ package com.mycompany.quickchat;
  * @author Student
  */
 public class Message {
+    boolean checkMessage() {
+        return true;
+    }
+    
+    String checkRecientCell() {
+        return "";
+    }
+    
+    String createMessageHash() {
+        return "";
+    }
+    
+    String sentMessage() {
+        return "";
+    }
+    
+    String printMessages() {
+        return "";
+    }
+    
+    int returnTotalMessages(){
+        return 1;
+    }
+    
+    //The POE said this function should use JSONs to store messages
+    void storeMessage() {
+    }
     
 }
