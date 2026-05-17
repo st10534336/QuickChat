c3eb52bdd9c631ded9adf78c6b684e018397a993
st10534336 <st10534336@rcconnect.edu.za>
2026-05-17 20:57:19 +0200

    Create messageTests.java

---
+++ src/test/java/messageTests.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author User
+ */
+public class messageTests {
+    
+    public messageTests() {
+    }
+    
+}
